a9249034920f739ed3ab4e71aa5dd3db9f467a23
Matthew Lake <matthew.lake@slicelife.com>
2024-11-29 00:18:17 -0500

    chore: add comment

--- team_size.py
+++ team_size.py
@@ -1,4 +1,7 @@
 class TeamSize:
+    # The purpose of this class is to calculate Team Size
+    # The formula is Team Size = PM / TDEV
+
     def __init__(self, effort_estimation, schedule_estimation):
         self.effort_estimation = effort_estimation
         self.schedule_estimation = schedule_estimation
